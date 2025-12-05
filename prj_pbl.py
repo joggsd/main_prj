a54ed64f8aad1fd44fd93a31ace98020614fcc30
DESKTOP-8CTAJ6U\쥐마 <jeonji1101@gmail.com>
2025-12-05 15:15:10 +0900

    add class

--- prj_pbl.py
+++ prj_pbl.py
@@ -3,3 +3,8 @@ import tkinter as tk
 from tkinter import ttk, messagebox, filedialog # GUI 위젯, 메시지 박스, 파일 대화상자
 import json # JSON 데이터 처리
 import pandas as pd # CSV, Excel 데이터 처리
+
+# 학생 정보 관리 앱 메인 클래스
+class StudentManagerApp:
+    # 초기화 메서드
+    def __init__(self, root):
